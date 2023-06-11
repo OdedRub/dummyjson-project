21b0af214f6e360844186b8f2b0f5944ed25e975
oded.rubinstein <odedrub@gmail.com>
2023-06-11 11:28:59 +0300

    added files

---
+++ README.md
@@ -0,0 +1,19 @@
+# Example API project
+
+This Python script uses the dummyjson website (`https://dummyjson.com`) to retrieve data using API requests.
+The user can choose to view one of three different data types - products, users or carts.
+
+## Requirements
+
+- Python 3.x
+- `requests` library.
+
+## Usage
+
+1. Clone the repository or download the script file (`dummyjson-api.py`).
+
+2. Install the `requests` library if it's not already installed, using `pip install requests` command.
+
+3. Run the script using `python3 dummyjson-api.py` command.
+
+4. Follow the on-screen prompts to select the data type and the corresponding ID in order to retrieve the requested data (prints on screen).
---
+++ dummyjson-api.py
@@ -0,0 +1,25 @@
+import requests  # imports requests library for making API requests
+
+endpoints = {  # defines a dictionary for the different options
+    "products": "https://dummyjson.com/products",
+    "carts": "https://dummyjson.com/carts",
+    "users": "https://dummyjson.com/users"
+}
+valid_options = set(endpoints.keys())  # defines a set for the available endpoints' keys
+
+json_type = input("Enter data type (products/carts/users): ")  # asks user for input
+
+if json_type in valid_options:  # checks if the input is one of the valid endpoints
+    data_id = input("Enter an ID (1-100 for products or users, 1-20 for carts) or press 0 to get all of them: ")
+    url = endpoints[json_type]  # sets 'url' to requested option
+
+    if data_id == "0":  # checks if ID input is 0
+        response = requests.get(url)  # sends GET request to requested enpoint
+        data = response.json()  # parses retrieved data to json
+        print(data)  # prints json
+    else:
+        response = requests.get(f"{url}/{data_id}")  # sends GET request to requested endpoint with id
+        data = response.json()  # parses retrieved data to json
+        print(data)  # prints json
+else:
+    print("Invalid input.")  # in case a different input was typed by user
